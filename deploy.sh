52eff21a950bf1fc6421e3d39b81e2602bee3026
Corey Burns <coreymburns@gmail.com>
2022-10-29 20:32:02 -0400

    test

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-echo What should the version be?
-read VERSION
-
-docker build -t coreymburns/social-frontend:$VERSION .
-docker push coreymburns/social-frontend:$VERSION
+docker build -t coreymburns/social-frontend .
+docker push coreymburns/social-frontend
 
 
 
